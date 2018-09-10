5654be759de7db6ef4878f1db4f5b01df4490e3e
Mark Black <32424631+markblack96@users.noreply.github.com>
2018-09-10 18:39:47 -0500

    Update weather_data_testing.py

--- weather_data_testing.py
+++ weather_data_testing.py
@@ -7,7 +7,8 @@ import matplotlib.dates as mdates
 import calendar
 
 
-months = ['01','02','03','04','05','06','07','08','09','10','11','12']
+months = ["%02d" % i for i in range(1,13)]
+full_months = calendar.month_name[1:13]
 month = lambda date: date[-2:]
 
 df = pd.read_csv("weather_averages.csv", index_col="DATE")
@@ -15,14 +16,16 @@ df = pd.read_csv("weather_averages.csv", index_col="DATE")
 tavgs = []
 year = 1940
 
-plt.magma()
-
 # df.loc[str(year) + '-' + months[i]] provides values for yyyy-mm via df.loc[]
 # TODO: update line color
+
 while year <= 2016:
-    for i in range(len(months)):
-        tavgs.append(df.loc[str(year) + '-' + months[i]].TAVG)
-    print(tavgs)
+    for date in df.index:
+        if str(year) in date:
+            tavgs.append(df.loc[str(year) + '-' + month(date)].TAVG)
+    #for i in range(len(months)):
+    #    tavgs.append(df.loc[str(year) + '-' + months[i]].TAVG)
+    tavgs = np.array(tavgs)
     plt.plot(months, tavgs)
     tavgs = []
     year += 1
@@ -30,11 +33,10 @@ while year <= 2016:
 # plt.axis([1,12,0,100])
 plt.title('Monthly average temperatures (F) in Shreveport, LA')
 plt.xlabel('Months')
-plt.xticks(np.arange(12), calendar.month_name[1:13], rotation=90)
+plt.xticks(np.arange(1, 13), full_months, rotation=45)
+
 # plt.legend(loc='upper left')
-from matplotlib import rcParams
-rcParams.update({'figure.autolayout': True})
-# plt.tight_layout()
+plt.grid()
 plt.show()
 
 #plt.plot(figures)
